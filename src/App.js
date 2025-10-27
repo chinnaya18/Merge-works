531fb0786339ef397ed797b34612ad8d1f52f972
chinnaya18 <chinnayagithub18@gmail.com>
2025-10-27 15:58:56 +0530

    Feat : Home page added (3) Files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import RightSidebarPopularRepos from "./RightSidebarPopularRepos";
+import Home from "./Home";
+import LeftSideBar from "./leftSideBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Home />
+      <LeftSideBar width="350px" headerHeight="60px" />
+      <RightSidebarPopularRepos width="350px" headerHeight="60px" />
     </div>
   );
 }
