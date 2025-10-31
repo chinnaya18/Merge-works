8951ac7ab2807b3540ff4bcd8a1e83ce0fabba0c
dheepthiramakrishnan <dheepthi.ramu@gmail.com>
2025-10-31 06:51:25 +0530

    added welcome page with logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import RightSidebarPopularRepos from "./RightSidebarPopularRepos";
 import Home from "./Home";
+import Welcome from "./Welcome";
 import LeftSideBar from "./leftSideBar";
 
 function App() {
   return (
     <div>
+      <Welcome >
       <Home />
       <LeftSideBar width="350px" headerHeight="60px" />
       <RightSidebarPopularRepos width="350px" headerHeight="60px" />
+    </Welcome>
+
     </div>
   );
 }
